440b76e9ace6bce964c4b2f68c758f9a2efe9d3b
CoderLast <52178799+Coderlast@users.noreply.github.com>
2021-03-07 12:00:46 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__."/vendor/autoload.php";
+
+//
+use App\CoderLast\Framework;
+use App\CoderLast\Plugins;
+use Ozdemir\Datatables\Datatables;
+use Ozdemir\Datatables\DB\MySQL;
+//
+$bot = new Framework($config['TOKEN']);
+$plugins = new Plugins($config['TOKEN']);
+$dt = new Datatables( new MySQL($config['mysql']) );
+
+$home = [["Hello word!", "Hello word!"]];
+
+$input = $bot->getInput();
+if(isset($input->message)){
+	$message = $input->message;
+	$chat_id = $message->chat->id;
+	$text = $message->text;
+	$userid	= $message->from->id;
+	$name = $message->from->frist_name." ".$message->from->last_name;
+}
+
+if($text == "/start"){
+	$bot->sendMessage($userid,"CLFramework - $name",[
+		'parse_mode'=>"markdown",
+		'reply_markup'=>$plugins->Keyboards($home)
+		]);
+}
+
+if($text == "Hello word!"){
+    $a = $bot->getMethod();
+    $bot->sendMessage($chat_id, print_r($a,true));
+}
+if($text == "Hello word !"){
+    $a = $plugins->getMethod();
+    $bot->sendMessage($chat_id, print_r($a,true));
+}
